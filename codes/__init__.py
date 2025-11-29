82aa30c63926143d4fcfb9d8f7afa198e4fa1fe1
Jonghan Ko <jonghan.ko@jnu.ac.kr>
2025-11-29 10:57:20 +0900

    Add files via upload

---
+++ codes/__init__.py
@@ -0,0 +1,8 @@
+"""
+VIs to LAI crops package
+Contains modules for simulating LAI from Vegetation Indices
+Includes both 1D time-series and 2D geospatial simulation modules
+"""
+
+__version__ = '1.0.0'
+
